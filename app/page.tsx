fc0c487cec4de7d09f3e93138960f352cca5ece7
kener <kcm.smart@gmail.com>
2026-02-09 18:10:14 -0500

    Se agrega la seccion de servicios y contacto

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Formulario from "@/components/Formulario";
 import Slider from "@/components/Slider";
 import { Icon } from "@iconify/react";
 import { Poppins } from "next/font/google";
@@ -15,55 +16,192 @@ export default function IndexPage() {
       <Slider />
 
       {/* Sobre Nosotros */}
-      <section aria-labelledby="sobre-nosotros" className="w-full mt-20 max-w-7xl mx-auto p-3 grid grid-cols-1 md:grid-cols-2">
+      <section
+        aria-labelledby="sobre-nosotros"
+        className="w-full mt-20 max-w-7xl mx-auto p-5 grid grid-cols-1 lg:grid-cols-2"
+      >
         <div className="flex flex-col">
           <Image
             src="/images/imagen_sobre_nosotros.webp"
             width={500}
             height={450}
+            fetchPriority="high"
+            sizes="(max-width: 768px) 217px, 300px"
             alt="Equipo de Aurum Customs SAC gestionando operaciones aduaneras"
             className="w-xl h-auto rounded-4xl shadow-lg border-b-8 border-l-8 border-acento"
           ></Image>
         </div>
 
         <div className="flex flex-col gap-4">
-          <h2 className="text-144px text-secundario   font-semibold">SOBRE NOSOTROS</h2>
+          <h2 className="text-14px text-secundario  border-l-2 px-1  font-semibold">
+            SOBRE NOSOTROS
+          </h2>
           <h3 className="text-3px lg:text-36px font-extrabold  text-secundario leading-tight">
             Más que una agencia, somos tu socio estratégico en comercio exterior
           </h3>
 
           <p className="text-16px text-gris leading-relaxed">
-            <strong className="text-secundario">Aurum Customs SAC</strong> es una agencia de aduanas en el Callao
-            especializada en importación, exportación y comercio exterior.
-            Brindamos agenciamiento de aduanas, asesoría estratégica y
-            soluciones complementarias, garantizando cumplimiento SUNAT y
-            operaciones seguras y eficientes.
+            <strong className="text-secundario">Aurum Customs SAC</strong> es
+            una agencia de aduanas en el Callao especializada en importación,
+            exportación y comercio exterior. Brindamos agenciamiento de aduanas,
+            asesoría estratégica y soluciones complementarias, garantizando
+            cumplimiento SUNAT y operaciones seguras y eficientes.
           </p>
 
-           {/* Beneficios */}
+          {/* Beneficios */}
           <div className="space-y-4 mt-5">
             <div className="flex items-start gap-3">
-              <Icon icon="lets-icons:check-fill" width="24" height="24" className="text-secundario" />
-              <p className="text-gris">Cumplimiento normativo y prevención de riesgos</p>
+              <Icon
+                icon="lets-icons:check-fill"
+                width="24"
+                height="24"
+                className="text-secundario"
+              />
+              <p className="text-gris">
+                Cumplimiento normativo y prevención de riesgos
+              </p>
             </div>
 
             <div className="flex items-start gap-3">
-             <Icon icon="lets-icons:check-fill" width="24" height="24" className="text-secundario" />
-              <p className="text-gris">Optimización de tiempos y costos operativos</p>
+              <Icon
+                icon="lets-icons:check-fill"
+                width="24"
+                height="24"
+                className="text-secundario"
+              />
+              <p className="text-gris">
+                Optimización de tiempos y costos operativos
+              </p>
             </div>
 
             <div className="flex items-start gap-3">
-              <Icon icon="lets-icons:check-fill" width="24" height="24" className="text-secundario" />
-              <p className="text-gris">Acompañamiento estratégico personalizado</p>
+              <Icon
+                icon="lets-icons:check-fill"
+                width="24"
+                height="24"
+                className="text-secundario"
+              />
+              <p className="text-gris">
+                Acompañamiento estratégico personalizado
+              </p>
             </div>
           </div>
-         
 
-          <Link href="/sobre-nosotros"
-          aria-label="Conoce nuestros servicios de Aduanas"
-          className="w-80 min-h-12 p-5 group   text-blanco text-14px bg-secundario mb-20 font-semibold uppercase hover:font-semibold mt-5 rounded-full flex justify-center gap-4 hover:scale-105 transition-transform duration-200 ease-in-out">
+          <Link
+            href="/sobre-nosotros"
+            aria-label="Conoce nuestros servicios de Aduanas"
+            className="w-80 min-h-12 p-5 group   text-blanco text-14px bg-secundario mb-20 font-semibold uppercase hover:font-semibold mt-5 rounded-full flex justify-center gap-4 hover:scale-105 transition-transform duration-200 ease-in-out"
+          >
             Conoce más sobre nosotros
           </Link>
+        </div>
+      </section>
+
+      {/* Nuestros servicios */}
+      <section
+        aria-labelledby="sobre-servicios"
+        className="w-full mt-20 max-w-7xl mx-auto p-5 flex flex-col items-center gap-4"
+      >
+        <h2 className="text-14px text-secundario border-l-2 px-1   font-semibold">
+          NUESTROS SERVICIOS
+        </h2>
+        <h3 className="text-3px lg:text-36px font-extrabold  text-secundario leading-tight">
+          Soluciones Integrales 360 en Agenciamiento de Aduanas
+        </h3>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-10">
+
+          
+          <div className="bg-blanco rounded-2xl shadow-lg p-6 hover:shadow-2xl  hover:scale-105 transition-transform duration-300">
+            <Link href="/servicios/agenciamiento-de-aduanas-especializado" className="flex flex-col items-center gap-4">
+              <Icon
+                icon="ic:round-local-shipping"
+                width="48"
+                height="48"
+                className="text-secundario"
+              />
+              <h4 className="text-20px font-bold text-secundario">
+                Agenciamiento de Aduanas Especializado
+              </h4>
+              <p className="text-gris text-center">
+                Gestión integral de operaciones aduaneras para importación y
+                exportación, asegurando cumplimiento normativo y eficiencia
+                operativa.
+              </p>
+            </Link>
+          </div>
+          <div  className="bg-blanco rounded-2xl shadow-lg p-6 hover:shadow-2xl  hover:scale-105 transition-transform duration-300">
+            <Link href="/servicios/especializado-asesoria-y-consultoria-en-comercio-exterior" className="flex flex-col items-center gap-4 text-center" >
+                <Icon
+                icon="ic:round-assessment"
+                width="48"
+                height="48"
+                className="text-secundario"
+              />
+              <h4 className="text-20px font-bold text-secundario">
+                Especializado Asesoría y Consultoría en Comercio Exterior
+              </h4>
+              <p className="text-gris text-center">
+                Asesoría estratégica personalizada en comercio exterior,
+                optimizando operaciones y garantizando cumplimiento normativo.
+              </p>
+            </Link>
+          </div>
+          <div className="bg-blanco rounded-2xl shadow-lg p-6 hover:shadow-2xl  hover:scale-105 transition-transform duration-300">
+            <Link href="/servicios/servicios-complementarios-a-su-disposicion" className="flex flex-col items-center gap-4">
+              <Icon
+              icon="ic:round-handshake"
+              width="48"
+              height="48"
+              className="text-secundario"
+            />
+            <h4 className="text-20px font-bold text-secundario">
+              Servicios complementarios a sudisposición
+            </h4>
+            <p className="text-gris text-center">
+              Servicios complementarios en comercio exterior, incluyendo
+              capacitación, auditorías y consultoría para optimizar operaciones.
+            </p>
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      {/* Contacto*/}
+
+      <section
+        aria-labelledby="contacto"
+        className="w-full mt-20 max-w-7xl mx-auto p-5 grid grid-cols-1 lg:grid-cols-2 items-center gap-10"
+      >
+        <div>
+          <h2 className="text-14px text-secundario border-l-2 px-1   font-semibold">
+            CONTACTO
+          </h2>
+          <h3 className="text-3px lg:text-36px mt-4 font-extrabold   text-secundario leading-tight">
+            Contáctanos para una cotización personalizada
+          </h3>
+          <p className="text-16px text-gris leading-relaxed mt-5">
+            ¿Listo para optimizar tus operaciones de comercio exterior?
+            Contáctanos hoy mismo para una cotización personalizada y descubre
+            cómo podemos ayudarte a alcanzar el éxito en tus operaciones
+            aduaneras.
+          </p>
+          <div className="mt-5">
+            <iframe
+            title="Mapa de ubicación Aurum Customs SAC"
+            src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d7804.6045198006705!2d-77.10518600000002!3d-12.0227!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTLCsDAxJzIxLjciUyA3N8KwMDYnMTguNyJX!5e0!3m2!1ses-419!2spe!4v1770673467375!5m2!1ses-419!2spe"
+            width="600"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen={true}
+            loading="lazy"
+            className="rounded-4xl"
+            referrerPolicy="strict-origin-when-cross-origin"
+          ></iframe>
+          </div>
+        </div>
+        <div>
+          <Formulario />
         </div>
       </section>
     </div>
--- app/sobre-nosotros/page.tsx
+++ app/sobre-nosotros/page.tsx
@@ -1,71 +1,87 @@
 import Image from "next/image"
-
+import { Icon } from "@iconify/react";
+import Link from "next/link";
 export default function NosotrosPage(){
     return(
-        <div>
-            <section className="bg-white py-20">
-      <div className="max-w-7xl mx-auto px-6 lg:px-12 grid lg:grid-cols-2 gap-16 items-center">
-        
-        {/* Imagen */}
-        <div className="relative">
-          <div className="relative w-full h-112.5 rounded-2xl overflow-hidden shadow-xl">
-            <Image
-              src="/equipo-aduanas.jpg"
-              alt="Equipo de Aurum Customs SAC gestionando operaciones aduaneras"
-              fill
-              className="object-cover"
-            />
-          </div>
-
-          {/* Detalle decorativo */}
-          <div className="absolute -bottom-6 -left-6 w-24 h-24 bg-cyan-400 rounded-2xl -z-10"></div>
+            <section
+        aria-labelledby="sobre-nosotros"
+        className="w-full mt-20 max-w-7xl mx-auto p-5 grid grid-cols-1 lg:grid-cols-2"
+      >
+        <div className="flex flex-col">
+          <Image
+            src="/images/imagen_sobre_nosotros.webp"
+            width={500}
+            height={450}
+            fetchPriority="high"
+            sizes="(max-width: 768px) 217px, 300px"
+            alt="Equipo de Aurum Customs SAC gestionando operaciones aduaneras"
+            className="w-xl h-auto rounded-4xl shadow-lg border-b-8 border-l-8 border-acento"
+          ></Image>
         </div>
 
-        {/* Texto */}
-        <div>
-          <span className="text-cyan-600 font-semibold uppercase tracking-wide text-sm">
-            Sobre Nosotros
-          </span>
-
-          <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mt-4 mb-6 leading-tight">
-            Más que una agencia, somos tu socio estratégico en comercio exterior
+        <div className="flex flex-col gap-4">
+          <h2 className="text-14px text-secundario  border-l-2 px-1  font-semibold">
+            SOBRE NOSOTROS
           </h2>
+          <h3 className="text-3px lg:text-36px font-extrabold  text-secundario leading-tight">
+            Más que una agencia, somos tu socio estratégico en comercio exterior
+          </h3>
 
-          <p className="text-gray-600 leading-relaxed mb-6">
-            <strong>Aurum Customs SAC</strong> es una agencia de aduanas en el Callao 
-            especializada en importación, exportación y agenciamiento de aduanas. 
-            Brindamos asesoría y consultoría en comercio exterior, garantizando 
-            cumplimiento normativo SUNAT y operaciones seguras.
+          <p className="text-16px text-gris leading-relaxed">
+            <strong className="text-secundario">Aurum Customs SAC</strong> es
+            una agencia de aduanas en el Callao especializada en importación,
+            exportación y comercio exterior. Brindamos agenciamiento de aduanas,
+            asesoría estratégica y soluciones complementarias, garantizando
+            cumplimiento SUNAT y operaciones seguras y eficientes.
           </p>
 
           {/* Beneficios */}
-          <div className="space-y-4 mb-8">
+          <div className="space-y-4 mt-5">
             <div className="flex items-start gap-3">
-              <div className="w-4 h-4 bg-cyan-500 rounded-full mt-1"></div>
-              <p className="text-gray-700">Cumplimiento normativo y prevención de riesgos</p>
+              <Icon
+                icon="lets-icons:check-fill"
+                width="24"
+                height="24"
+                className="text-secundario"
+              />
+              <p className="text-gris">
+                Cumplimiento normativo y prevención de riesgos
+              </p>
             </div>
 
             <div className="flex items-start gap-3">
-              <div className="w-4 h-4 bg-cyan-500 rounded-full mt-1"></div>
-              <p className="text-gray-700">Optimización de tiempos y costos operativos</p>
+              <Icon
+                icon="lets-icons:check-fill"
+                width="24"
+                height="24"
+                className="text-secundario"
+              />
+              <p className="text-gris">
+                Optimización de tiempos y costos operativos
+              </p>
             </div>
 
             <div className="flex items-start gap-3">
-              <div className="w-4 h-4 bg-cyan-500 rounded-full mt-1"></div>
-              <p className="text-gray-700">Acompañamiento estratégico personalizado</p>
+              <Icon
+                icon="lets-icons:check-fill"
+                width="24"
+                height="24"
+                className="text-secundario"
+              />
+              <p className="text-gris">
+                Acompañamiento estratégico personalizado
+              </p>
             </div>
           </div>
 
-          <a
-            href="/nosotros"
-            className="inline-block bg-cyan-600 hover:bg-cyan-700 text-white px-6 py-3 rounded-full font-medium transition shadow-md"
+          <Link
+            href="/sobre-nosotros"
+            aria-label="Conoce nuestros servicios de Aduanas"
+            className="w-80 min-h-12 p-5 group   text-blanco text-14px bg-secundario mb-20 font-semibold uppercase hover:font-semibold mt-5 rounded-full flex justify-center gap-4 hover:scale-105 transition-transform duration-200 ease-in-out"
           >
             Conoce más sobre nosotros
-          </a>
+          </Link>
         </div>
-
-      </div>
-    </section>
-        </div>
+      </section>
     )
 }
